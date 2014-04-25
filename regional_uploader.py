e31a4d352bd88fc8fe449861293bf8365210a187
Roxly Rivero <roxly@auberonsolutions.com>
2014-04-25 16:28:29 +0800

    refs #1095 disabled form view,

--- regional_uploader.py
+++ regional_uploader.py
@@ -74,61 +74,48 @@ class regional_upload(osv.osv_memory):
             raise osv.except_osv(_('Error !'), _('Make sure that all entries has a corresponding entry!'))
         return {'trans_nbr': count, 'credit': credit, 'debit': debit}
 
-    
-    def upload(self, cr, uid, ids, context=None):
-        regional_entries = self.pool.get('regional.uploader').search(cr, uid, [('uploaded','=',False)])
-	period_id = False
-	date_now = False
-	for form in self.read(cr, uid, ids, context=None):
-	    period_id = form['period_id']
-	    date_now = form['date']
-        if regional_entries:
-            date = datetime.datetime.now()
-            journal_search = self.pool.get('account.journal').search(cr, uid, [('type','=','regional_report')],limit=1)
-            journal_id = False
-            for journal in journal_search:
-                journal_id = journal
+    def uploadData(self, cr, uid, ids, context=None):
+        for form in self.read(cr, uid, ids, context=None):
+            periodReader = self.pool.get('account.period').read(cr, uid, form['period_id'],['date_start','date_stop'])
             move = {
-                'journal_id':journal_id,
-                'period_id':period_id,
-                'date':date_now
+                'journal_id':form['journal_id'],
+                'period_id':form['period_id'],
+                'date':form['date'],
+                'ref':form['name'],
                 }
             move_id = self.pool.get('account.move').create(cr, uid, move)
-            for regional_entry in regional_entries:
+            for trans_id in context['active_ids']:
+                transactionReader = self.pool.get('regional.uploader').read(cr, uid, trans_id, context=None)
                 curr_id = False
                 account_id = False
                 analytic_id = False
-                account_search = False
-                analytic_search = False
-                regional_entries_read = self.pool.get('regional.uploader').read(cr, uid, regional_entry,context=None)
-                print regional_entries_read
-                account_search = self.pool.get('account.accpac').search(cr, uid,[('name','=',regional_entries_read['acctid'])])
-                accpac_acc = regional_entries_read['acctid']
-                if not account_search:
-                    accSearchNormal = self.pool.get('account.account').search(cr, uid, [('code','=',regional_entries_read['acctid'])])
+                checkAccpacAccount = self.pool.get('account.accpac').search(cr, uid, [('name','=',transactionReader['acctid'])])
+                accpac_acc = transactionReader['acctid']
+                if not checkAccpacAccount:
+                    accSearchNormal = self.pool.get('account.account').search(cr, uid, [('code','=',transactionReader['acctid'])], limit=1)
                     if not accSearchNormal:
-                        accSearchAnalytic = self.pool.get('account.analytic.account').search(cr, uid, [('code','=',regional_entries_read['acctid'])])
+                        accSearchAnalytic = self.pool.get('account.analytic.account').search(cr, uid, [('code','=',transactionReader['acctid'])], limit=1)
                         if not accSearchAnalytic:
-                            raise osv.except_osv(_('Error !'), _('Account %s is not on the list of any accounts!')%accpac_acc)
+                            raise osv.except_osv(_('Error !'), _('RU-001: Account %s is not on the list of any accounts!')%accpac_acc)
                         elif accSearchAnalytic:
                             analyticRead = self.pool.get('account.analytic.account').read(cr, uid, accSearchAnalytic[0],['normal_account'])
                             analytic_id = accSearchAnalytic[0]
                             account_id = analyticRead['normal_account'][0]
                     elif accSearchNormal:
                         account_id = accSearchNormal[0]
-                elif account_search:
-                    acc_read = self.pool.get('account.accpac').read(cr, uid, account_search[0], context=None)
+                elif checkAccpacAccount:
+                    acc_read = self.pool.get('account.accpac').read(cr, uid, checkAccpacAccount[0], context=None)
                     if acc_read['account_id']:
                         account_id = acc_read['account_id'][0]
                     elif acc_read['analytic_id']:
                         analyticRead = self.pool.get('account.analytic.account').read(cr, uid, acc_read['analytic_id'][0],['normal_account'])
                         analytic_id = acc_read['analytic_id'][0]
                         account_id = analyticRead['normal_account'][0]
-                curr_search = self.pool.get('res.currency').search(cr, uid, [('name','ilike',regional_entries_read['currency'])])
+                curr_search = self.pool.get('res.currency').search(cr, uid, [('name','ilike',transactionReader['currency'])])
                 for currency in curr_search:
                     curr_id = currency
                 currency_read = self.pool.get('res.currency').read(cr, uid, curr_id, ['rate'])
-                amount = regional_entries_read['scuramt'] / currency_read['rate']
+                amount = transactionReader['scuramt'] / currency_read['rate']
                 credit = 0.00
                 debit = 0.00
                 if amount > 0.00:
@@ -137,23 +124,106 @@ class regional_upload(osv.osv_memory):
                 elif amount < 0.00:
                     credit = amount * -1
                     debit = 0.00
-                name = regional_entries_read['transdesc'] +' '+regional_entries_read['date']
-		move_line = {
-                    'name':name,
+                if transactionReader['date']<periodReader['date_start'] or transactionReader['date']>periodReader['date_stop']:
+                    raise osv.except_osv(_('Error !'), _('RU-002: The date of the transaction is not within the period!'))
+                else:
+                    name = transactionReader['transdesc'] +' '+transactionReader['date']
+                    move_line = {
+                        'name':name,
+                        'journal_id':form['journal_id'],
+                        'period_id':form['period_id'],
+                        'account_id':account_id,
+                        'credit':credit,
+                        'debit':debit,
+                        'analytic_account_id':analytic_id,
+                        'date':transactionReader['date'],
+                        'ref':transactionReader['ref'],
+                        'move_id':move_id,
+                        'amount_currency':transactionReader['scuramt'],
+                        'currency_id':curr_id,
+                        }
+                    self.pool.get('account.move.line').create(cr, uid, move_line)
+        return {'type': 'ir.actions.act_window_close'}
+
+    def upload2(self, cr, uid, ids, context=None):
+        regional_entries = self.pool.get('regional.uploader').search(cr, uid, [('uploaded','=',False)])
+        period_id = False
+        date_now = False
+        for form in self.read(cr, uid, ids, context=None):
+            period_id = form['period_id']
+            date_now = form['date']
+            if regional_entries:
+                date = datetime.datetime.now()
+                journal_search = self.pool.get('account.journal').search(cr, uid, [('type','=','regional_report')],limit=1)
+                journal_id = False
+                for journal in journal_search:
+                    journal_id = journal
+                move = {
                     'journal_id':journal_id,
                     'period_id':period_id,
-                    'account_id':account_id,
-                    'credit':credit,
-                    'debit':debit,
-                    'analytic_account_id':analytic_id,
-                    'date':regional_entries_read['date'],
-                    'ref':regional_entries_read['ref'],
-                    'move_id':move_id,
-                    'amount_currency':regional_entries_read['scuramt'],
-                    'currency_id':curr_id,
+                    'date':date_now
                     }
-                self.pool.get('account.move.line').create(cr, uid, move_line)
-                self.pool.get('regional.uploader').write(cr, uid, regional_entry, {'uploaded':True})
-            #self.pool.get('account.move').post(cr, uid, [move_id])
+                move_id = self.pool.get('account.move').create(cr, uid, move)
+                for regional_entry in regional_entries:
+                    curr_id = False
+                    account_id = False
+                    analytic_id = False
+                    account_search = False
+                    analytic_search = False
+                    regional_entries_read = self.pool.get('regional.uploader').read(cr, uid, regional_entry,context=None)
+                    print regional_entries_read
+                    account_search = self.pool.get('account.accpac').search(cr, uid,[('name','=',regional_entries_read['acctid'])])
+                    accpac_acc = regional_entries_read['acctid']
+                    if not account_search:
+                        accSearchNormal = self.pool.get('account.account').search(cr, uid, [('code','=',regional_entries_read['acctid'])])
+                        if not accSearchNormal:
+                            accSearchAnalytic = self.pool.get('account.analytic.account').search(cr, uid, [('code','=',regional_entries_read['acctid'])])
+                            if not accSearchAnalytic:
+                                raise osv.except_osv(_('Error !'), _('Account %s is not on the list of any accounts!')%accpac_acc)
+                            elif accSearchAnalytic:
+                                analyticRead = self.pool.get('account.analytic.account').read(cr, uid, accSearchAnalytic[0],['normal_account'])
+                                analytic_id = accSearchAnalytic[0]
+                                account_id = analyticRead['normal_account'][0]
+                        elif accSearchNormal:
+                            account_id = accSearchNormal[0]
+                    elif account_search:
+                        acc_read = self.pool.get('account.accpac').read(cr, uid, account_search[0], context=None)
+                        if acc_read['account_id']:
+                            account_id = acc_read['account_id'][0]
+                        elif acc_read['analytic_id']:
+                            analyticRead = self.pool.get('account.analytic.account').read(cr, uid, acc_read['analytic_id'][0],['normal_account'])
+                            analytic_id = acc_read['analytic_id'][0]
+                            account_id = analyticRead['normal_account'][0]
+                    curr_search = self.pool.get('res.currency').search(cr, uid, [('name','ilike',regional_entries_read['currency'])])
+                    for currency in curr_search:
+                        curr_id = currency
+                    currency_read = self.pool.get('res.currency').read(cr, uid, curr_id, ['rate'])
+                    amount = regional_entries_read['scuramt'] / currency_read['rate']
+                    credit = 0.00
+                    debit = 0.00
+                    if amount > 0.00:
+                        credit = 0.00
+                        debit = amount
+                    elif amount < 0.00:
+                        credit = amount * -1
+                        debit = 0.00
+                    name = regional_entries_read['transdesc'] +' '+regional_entries_read['date']
+                    move_line = {
+                        'name':name,
+                        'journal_id':journal_id,
+                        'period_id':period_id,
+                        'account_id':account_id,
+                        'credit':credit,
+                        'debit':debit,
+                        'analytic_account_id':analytic_id,
+                        'date':regional_entries_read['date'],
+                        'ref':regional_entries_read['ref'],
+                        'move_id':move_id,
+                        'amount_currency':regional_entries_read['scuramt'],
+                        'currency_id':curr_id,
+                        }
+                    self.pool.get('account.move.line').create(cr, uid, move_line)
+                    self.pool.get('regional.uploader').write(cr, uid, regional_entry, {'uploaded':True})
+                #self.pool.get('account.move').post(cr, uid, [move_id])
         return {'type': 'ir.actions.act_window_close'}
 regional_upload()
